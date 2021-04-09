93eeb19ff91432de05ee95d16532a17576ffb1e8
Jack C <30955604+jacany@users.noreply.github.com>
2021-04-09 17:58:04 -0400

    Added message edit listener

---
+++ src/listeners/Messages/edit.ts
@@ -0,0 +1,52 @@
+import { Listener, AkairoClient } from "discord-akairo";
+import { Message, MessageEmbed } from "discord.js";
+
+export default class MessageEditListener extends Listener {
+	client: AkairoClient;
+
+	constructor(client: AkairoClient) {
+		super("messageUpdate", {
+			emitter: "client",
+			event: "messageUpdate",
+		});
+
+		this.client = client;
+	}
+
+	exec(oldMessage: Message, newMessage: Message) {
+		if (!oldMessage.guild) return;
+		const logChannel = this.client.settings.get(
+			oldMessage.guild.id,
+			"loggingChannel",
+			"None"
+		);
+		if (logChannel === "None") return;
+		return (
+			// @ts-ignore
+			this.client.channels.cache
+				.get(logChannel)
+				// @ts-ignore
+				.send(
+					new MessageEmbed({
+						title: "Message Edited",
+						description:
+							"```diff" +
+							`\n- "${oldMessage.content}"` +
+							`\n+ "${newMessage.content}"` +
+							"\n```",
+						color: 16716032,
+						timestamp: new Date(),
+						author: {
+							name: oldMessage.author.tag,
+							icon_url: oldMessage.author.avatarURL({ dynamic: true }) || "",
+						},
+						footer: {
+							text: `No content ("") means that there was probably an embed there\n${oldMessage.client.user?.tag}`,
+							icon_url:
+								oldMessage.client.user?.avatarURL({ dynamic: true }) || "",
+						},
+					})
+				)
+		);
+	}
+}
