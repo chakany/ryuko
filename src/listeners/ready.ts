ee41df2ec95c336c501727d30396d1a7f0a922ca
Jack C <30955604+jacany@users.noreply.github.com>
2021-04-30 20:16:01 -0400

    i think this will fix a sharding issue

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -22,83 +22,85 @@ export default class ReadyListener extends Listener {
 		// Schedule Jobs
 		const guilds = await db.getMutedUsers();
 		for (const [guild, muted] of guilds) {
-			let current = outer.client.settings.get(guild, "mutedUsers", null);
-			if (typeof current === "string") current = new Map(JSON.parse(current));
-			else current = new Map(current);
-			let guildJobs = new Map();
-			for (const [id, expiry] of muted) {
-				const expires = new Date(expiry);
-				if (expires <= new Date()) {
-					current.delete(id);
-				} else {
-					const job = schedule.scheduleJob(expires, async function () {
-						const cachedGuild = await outer.client.guilds.cache.get(guild);
+			if (await outer.client.guilds.cache.get(guild)) {
+				let current = outer.client.settings.get(guild, "mutedUsers", null);
+				if (typeof current === "string") current = new Map(JSON.parse(current));
+				else current = new Map(current);
+				let guildJobs = new Map();
+				for (const [id, expiry] of muted) {
+					const expires = new Date(expiry);
+					if (expires <= new Date()) {
+						current.delete(id);
+					} else {
+						const job = schedule.scheduleJob(expires, async function () {
+							const cachedGuild = await outer.client.guilds.cache.get(guild);
 
-						const user = await cachedGuild?.members.fetch(id);
-						if (user === undefined) {
-							// Remove the user from DB.
-						}
-						const muteRole = outer.client.settings.get(
-							cachedGuild!.id,
-							"muteRole",
-							null
-						);
-						if (user!.roles.cache.has(muteRole))
-							// @ts-ignore
-							user.roles.remove(cachedGuild?.roles.cache.get(muteRole));
-
-						outer.client.jobs.get(cachedGuild!.id)?.delete(user!.id);
-						let cachedMutes = outer.client.settings.get(
-							cachedGuild!.id,
-							"mutedUsers",
-							null
-						);
-						if (typeof cachedMutes === "string")
-							cachedMutes = new Map(JSON.parse(cachedMutes));
-						else cachedMutes = new Map(cachedMutes);
-
-						cachedMutes.delete(id);
-
-						outer.client.settings.set(
-							guild,
-							"mutedUsers",
-							// @ts-ignore
-							JSON.stringify([...cachedMutes])
-						);
-
-						const logChannel = outer.client.settings.get(
-							guild,
-							"loggingChannel",
-							null
-						);
-
-						if (logChannel)
-							cachedGuild?.channels.cache
-								.get(`${logChannel}`)
+							const user = await cachedGuild?.members.fetch(id);
+							if (user === undefined) {
+								// Remove the user from DB.
+							}
+							const muteRole = outer.client.settings.get(
+								cachedGuild!.id,
+								"muteRole",
+								null
+							);
+							if (user!.roles.cache.has(muteRole))
 								// @ts-ignore
-								?.send(
-									new MessageEmbed({
-										title: "Member Unmuted",
-										description: `${user}'s mute has expired.`,
-										color: 16716032,
-										timestamp: new Date(),
-										footer: {
-											text: outer.client.user?.tag,
-											icon_url:
-												outer.client.user?.avatarURL({ dynamic: true }) || "",
-										},
-									})
-								);
-					});
-					guildJobs.set(id, job);
+								user.roles.remove(cachedGuild?.roles.cache.get(muteRole));
+
+							outer.client.jobs.get(cachedGuild!.id)?.delete(user!.id);
+							let cachedMutes = outer.client.settings.get(
+								cachedGuild!.id,
+								"mutedUsers",
+								null
+							);
+							if (typeof cachedMutes === "string")
+								cachedMutes = new Map(JSON.parse(cachedMutes));
+							else cachedMutes = new Map(cachedMutes);
+
+							cachedMutes.delete(id);
+
+							outer.client.settings.set(
+								guild,
+								"mutedUsers",
+								// @ts-ignore
+								JSON.stringify([...cachedMutes])
+							);
+
+							const logChannel = outer.client.settings.get(
+								guild,
+								"loggingChannel",
+								null
+							);
+
+							if (logChannel)
+								cachedGuild?.channels.cache
+									.get(`${logChannel}`)
+									// @ts-ignore
+									?.send(
+										new MessageEmbed({
+											title: "Member Unmuted",
+											description: `${user}'s mute has expired.`,
+											color: 16716032,
+											timestamp: new Date(),
+											footer: {
+												text: outer.client.user?.tag,
+												icon_url:
+													outer.client.user?.avatarURL({ dynamic: true }) || "",
+											},
+										})
+									);
+						});
+						guildJobs.set(id, job);
+					}
+					this.client.jobs.set(guild, guildJobs);
+					this.client.settings.set(
+						guild,
+						"mutedUsers",
+						// @ts-ignore
+						JSON.stringify([...current])
+					);
 				}
-				this.client.jobs.set(guild, guildJobs);
-				this.client.settings.set(
-					guild,
-					"mutedUsers",
-					// @ts-ignore
-					JSON.stringify([...current])
-				);
 			}
 		}
 
