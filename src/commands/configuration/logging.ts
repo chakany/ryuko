54902c3f02a223d3624438102352adb707469e05
Jack C <30955604+jacany@users.noreply.github.com>
2021-05-02 14:56:01 -0400

    disable logging

--- src/commands/configuration/logging.ts
+++ src/commands/configuration/logging.ts
@@ -1,4 +1,4 @@
-import { Command } from "discord-akairo";
+import { Command, Argument } from "discord-akairo";
 import { Message, MessageEmbed } from "discord.js";
 
 import Error from "../../utils/error";
@@ -6,16 +6,16 @@ import Error from "../../utils/error";
 const arg = [
 	{
 		id: "channel",
-		type: "textChannel",
+		type: Argument.union("textChannel", "string"),
 	},
 ];
 
-export default class LogchannelCommand extends Command {
+export default class LoggingCommand extends Command {
 	protected args = arg;
 
 	constructor() {
-		super("logchannel", {
-			aliases: ["logchannel", "loggingchannel"],
+		super("logging", {
+			aliases: ["logging", "log", "logchannel", "loggingchannel"],
 			category: "Configuration",
 			args: arg,
 			description: "Changes the Logging Channel",
@@ -25,6 +25,25 @@ export default class LogchannelCommand extends Command {
 	}
 
 	async exec(message: Message, args: any): Promise<any> {
+		if (args.channel === "disable" || args.channel === "off") {
+			this.client.settings.set(message.guild!.id, "loggingChannel", null);
+			return message.channel.send(
+				new MessageEmbed({
+					title: "Disabled Logging",
+					color: 16716032,
+					description: "Logging is now **off**",
+					timestamp: new Date(),
+					author: {
+						name: message.author.tag,
+						icon_url: message.author.avatarURL({ dynamic: true }) || "",
+					},
+					footer: {
+						text: message.client.user?.tag,
+						icon_url: message.client.user?.avatarURL({ dynamic: true }) || "",
+					},
+				})
+			);
+		}
 		const prefix = message.util?.parsed?.prefix;
 
 		// The third param is the default.
@@ -57,9 +76,7 @@ export default class LogchannelCommand extends Command {
 					message,
 					this,
 					"Invalid Configuration",
-					"There is no logging channel set, use the '" +
-						prefix +
-						"logchannel' command to set it."
+					"There is no logging channel set."
 				)
 			);
 		}
