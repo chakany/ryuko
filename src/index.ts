2dc2f61ccf00ed2e31b4bfff053a72bf9d3e5184
Jack C <30955604+jacany@users.noreply.github.com>
2021-05-06 15:11:33 -0400

    Readd old EJS template

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import { ShardingManager } from "discord.js";
 import bunyan from "bunyan";
+import express from "express";
 import db from "./utils/db";
-const { token } = require("../config.json");
+const { token, port } = require("../config.json");
 let log = bunyan.createLogger({ name: "shardmanager" });
 
 db.sync();
 
 let manager: ShardingManager;
-if (require.main === module) {
-	if (process.env.NODE_ENV !== "production")
-		manager = new ShardingManager("./bot.ts", {
-			token: token,
-			execArgv: ["-r", "ts-node/register"],
-		});
-	else
-		manager = new ShardingManager("./bot.js", {
-			token: token,
-		});
-	manager.on("shardCreate", (shard) => log.info(`Launched shard ${shard.id}`));
-	manager.spawn();
-}
+if (process.env.NODE_ENV !== "production")
+	manager = new ShardingManager("./bot.ts", {
+		token: token,
+		execArgv: ["-r", "ts-node/register"],
+	});
+else
+	manager = new ShardingManager("./bot.js", {
+		token: token,
+	});
+manager.on("shardCreate", (shard) => log.info(`Launched shard ${shard.id}`));
+manager.spawn();
+
+const app = express();
+
+import home from "./routes/home";
+
+app.set("view engine", "ejs");
+app.set("views", "../app/pages");
+
+app.use("/", home);
+
+app.use(express.static("../app/static"));
+
+app.listen(port, () => {
+	log.info(`Bound to port ${port}`);
+});
 
 export { manager, log };
---
+++ src/routes/home.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import { manager, log } from "../index";
+
+const router = express.Router();
+router.get("/", async function (req, res) {
+	try {
+		res.render("index", {
+			totalShards: manager.totalShards,
+			totalServers: await manager.fetchClientValues("guilds.cache.size"),
+			avatar: await (await manager.fetchClientValues("user"))[0].avatarURL,
+			username: await (await manager.fetchClientValues("user"))[0].username,
+		});
+	} catch (err) {
+		log.error(err);
+	}
+});
+
+export default router;
