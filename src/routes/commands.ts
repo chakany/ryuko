a7961a59c5efcfa0ecf91556774c7e8d6fc0e3f5
Jack C <jack@chaker.net>
2021-08-01 02:21:20 -0400

    Remake Commands Page

--- src/routes/commands.ts
+++ src/routes/commands.ts
@@ -8,7 +8,17 @@ import { manager, weblog, user } from "../index";
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-	res.redirect("commands/Configuration/disable");
+	try {
+		res.redirect("commands/Configuration");
+	} catch (err) {
+		weblog.error(err);
+		return res.status(500).render("error", {
+			username: user.username,
+			avatar: user.avatarURL,
+			code: 500,
+			description: "Internal Server Error",
+		});
+	}
 });
 
 router.get("/:category", async function (req, res) {
@@ -38,8 +48,6 @@ router.get("/:category", async function (req, res) {
 				)[0],
 				username: user.username,
 				avatar: user.avatarURL,
-				prefix: prefix,
-				command: null,
 				category: await (
 					await manager.fetchClientValues("commandHandler.categories")
 				)[0].find(
@@ -71,104 +79,6 @@ router.get("/:category", async function (req, res) {
 
 			res.sendFile(
 				`${process.cwd()}/pages/commands/${req.params.category.toLowerCase()}/theFuckingIndex123131312.html`
-			);
-		}
-	} catch (err) {
-		weblog.error(err);
-	}
-});
-
-router.get("/:category/:command", async function (req, res) {
-	try {
-		if (process.env.NODE_ENV !== "production") {
-			if (
-				!(await (
-					await manager.fetchClientValues("commandHandler.categories")
-				)[0].find(
-					(category: any) =>
-						category[0] &&
-						category[0].categoryID ===
-							req.params.category.charAt(0).toUpperCase() +
-								req.params.category.slice(1)
-				)) ||
-				!(await (
-					await manager.fetchClientValues("commandHandler.categories")
-				)[0]
-					.find(
-						(category: any) =>
-							category[0] &&
-							category[0].categoryID ===
-								req.params.category.charAt(0).toUpperCase() +
-									req.params.category.slice(1)
-					)
-					.find(
-						(command: Command) => command.id === req.params.command
-					))
-			)
-				// If the command or category doesn't exist
-				return res.status(404).render("error", {
-					username: user.username,
-					avatar: user.avatarURL,
-					code: 404,
-					description: "Page not Found",
-				});
-			res.render("commands", {
-				categories: (
-					await manager.fetchClientValues("commandHandler.categories")
-				)[0],
-				username: user.username,
-				avatar: user.avatarURL,
-				prefix: prefix,
-				command: await (
-					await manager.fetchClientValues("commandHandler.categories")
-				)[0]
-					.find(
-						(category: any) =>
-							category[0] &&
-							category[0].categoryID ===
-								req.params.category.charAt(0).toUpperCase() +
-									req.params.category.slice(1)
-					)
-					.find(
-						(command: Command) => command.id === req.params.command
-					),
-			});
-		} else {
-			if (
-				!(await (
-					await manager.fetchClientValues("commandHandler.categories")
-				)[0].find(
-					(category: any) =>
-						category[0] &&
-						category[0].categoryID ===
-							req.params.category.charAt(0).toUpperCase() +
-								req.params.category.slice(1)
-				)) ||
-				!(await (
-					await manager.fetchClientValues("commandHandler.categories")
-				)[0]
-					.find(
-						(category: any) =>
-							category[0] &&
-							category[0].categoryID ===
-								req.params.category.charAt(0).toUpperCase() +
-									req.params.category.slice(1)
-					)
-					.find(
-						(command: Command) => command.id === req.params.command
-					))
-			)
-				return res.status(404).render("error", {
-					username: user.username,
-					avatar: user.avatarURL,
-					code: 404,
-					description: "Page not Found",
-				});
-
-			res.sendFile(
-				`${process.cwd()}/pages/commands/${req.params.category.toLowerCase()}/${
-					req.params.command
-				}.html`
 			);
 		}
 	} catch (err) {
